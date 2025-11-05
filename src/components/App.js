c2b13c7f85f7653f6610acd94659c14756182c27
Imhrutik05 <hrutikchaudhari5@gmail.com>
2025-11-05 08:32:55 +0530

    Task Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,21 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
+const projects = [
+  { name: "AutoHire", description: "Automatic Job Application Agent"},
+  { name: "Retainix", description: "Student dropout risk predictor"},
+  { name: "Mockmate", description: "AI Mock Interview Application"}
+]
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      {projects.map((project, index) => (
+        <div key={index} className="ns-wrapper">
+            <h1 data-ns-test="project-name">{project.name}</h1>
+            <h6 data-ns-test="project-name">{project.description}</h6>
+        </div>
+      ))}
+    </div>
   )
 }
 
